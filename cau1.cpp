c4ecab30a0f7852a45d3fec78776066d46a76501
emtoangithub <tn904375@gmail.com>
2022-04-19 21:37:33 +0700

    Deo biet thay doi cl gi luôn

--- cau1.cpp
+++ cau1.cpp
@@ -13,6 +13,8 @@ class sophuc
     friend sophuc operator - (sophuc, sophuc);
     friend sophuc operator * (sophuc, sophuc);
     friend sophuc operator / (sophuc, sophuc);
+    friend bool operator == (sophuc, sophuc);
+    friend bool operator != (sophuc, sophuc);
 };
 sophuc :: sophuc(double a, double b)
 : thuc(a), ao(b)
@@ -50,8 +52,24 @@ sophuc operator * (sophuc a, sophuc b)
     c.ao = a.ao*b.thuc + a.thuc*b.ao;
     return c;
 }
-
-
+sophuc operator / (sophuc a, sophuc b)
+{
+    sophuc c;
+    double temp0 = pow(b.ao ,2) + pow(b.thuc ,2);
+    double temp1 = a.thuc*b.thuc + a.ao*b.ao;
+    double temp2 = b.thuc*a.ao - a.thuc*b.ao;
+    c.thuc = temp1/temp0;
+    c.ao = temp2/temp0;  
+    return c;
+}
+bool operator == (sophuc a, sophuc b)
+{
+    return a.ao == b.ao && a.thuc == b.thuc;
+}
+bool operator != (sophuc a, sophuc b)
+{
+    return !(a==b);
+}
 int main()
 {
     sophuc a, b, c;
